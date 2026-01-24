71d5bba721b3a33232ed26479c607e53ec7c4dc7
Samuel Silva <samuka.santos.developer@gmail.com>
2026-01-23 22:31:55 -0300

    Exercício 22

---
+++ exercícios/ex022.py
@@ -0,0 +1,21 @@
+'''nome = input('Digite seu nome completo: ').strip()
+
+print('Analisando seu nome...')
+
+print('Seu nome em maiúsculas é', nome.upper())
+print('Seu nome em minúsculas é', nome.lower())
+
+# Remove os espaços para contar apenas letras
+print('Seu nome tem ao todo', len(nome.replace(' ', '')), 'letras')
+
+# Pega o primeiro nome
+primeiro_nome = nome.split()[0]
+print('Seu primeiro nome é', primeiro_nome, 'e ele tem', len(primeiro_nome), 'letras')'''
+
+nome = str(input('Digite seu nome completo: ')).strip()
+print('Analisando seu nome...')
+print('Seu nome em maiúsculas é {}.'.format(nome.upper()))
+print('Seu nome em minúsculas é {}.'.format(nome.lower()))
+print('Seu nome tem ao todo {} letras'.format(len(nome) - nome.count(' ')))
+separa = nome.split()
+print('Seu primeiro nome é {} e ele tem {} letras'.format(separa[0], len(separa[0])))
