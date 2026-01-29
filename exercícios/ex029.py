5e43c1bec7bbaac5ef9595593c791467ad2cae66
Samuel Silva <samuka.santos.developer@gmail.com>
2026-01-28 22:38:35 -0300

    Exercício 29

---
+++ exercícios/ex029.py
@@ -0,0 +1,13 @@
+'''velocidade = int(input('Qual a velocidade do carro? '))
+if velocidade > 80:
+    print('MULTADO! Você excedeu o limite permitido que é de 80Km/h')
+    velocidade = velocidade - 80
+else:
+    print('Tenha um bom dia! Dirija com segurança!')'''
+
+velocidade = float(input('Qual a velocidade do carro? '))
+if velocidade > 80:
+    print('MULTADO! Você excedeu o limite permitido que é de 80Km/h')
+    multa = (velocidade-80) * 7
+    print('Você deve pagar uma multa de R${:.2f}'.format(multa))
+print('Tenha um bom dia! Dirija com segurança!')
