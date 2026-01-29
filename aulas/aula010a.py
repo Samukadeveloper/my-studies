e796dbc9ca9098cba89ea82b3da1da35f41c51aa
Samuel Silva <samuka.santos.developer@gmail.com>
2026-01-28 21:56:44 -0300

    Aula 10

---
+++ aulas/aula010a.py
@@ -0,0 +1,22 @@
+'''tempo = int(input('Quantos anos tem seu carro?'))
+if tempo <= 3:
+    print('Carro novo')
+else:
+    print('Carro novo')
+print('--FIM--')'''
+
+'''nome = str(input('Qual é seu nome? '))
+if nome == 'Samuel':
+    print('Que nome lindo você tem!')
+else:
+    print('Huum, nome bem comum')
+print('Bom dia, {}!'.format(nome))'''
+
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+media = (n1 + n2) / 2
+print('A sua média foi {}'.format(media))
+if media >= 7:
+    print('Aprovado')
+else:
+    print('Reprovado')
